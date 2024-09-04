f593d4ccfbae2d9b964268c5baf45a562d734b7d
montserdabbas <maaldabbas155@gmail.com>
2024-09-04 20:35:03 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Products from './Components/Products';
+
+export default function App() {
+  return (
+    <>
+      
+      <Products/>
+    </>
+  );
+}
---
+++ src/Components/Products.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import style from "./Products.module.css";
+
+export default function Products() {
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = async () => {
+    const response = await fetch("https://forkify-api.herokuapp.com/api/search?q=pizza"); 
+    const data = await response.json();
+    setRecipes(data.recipes); 
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  return (
+    <div className='container'>
+      <div className='row g-3'>
+        {recipes.map(recipe => (
+          <div className='col-lg-3' >
+            <div className='card'>
+              <h2 className={`${style.fontsize}`}>{recipe.title.split(' ').slice(0, 3).join(' ')}</h2>
+              <img src={recipe.image_url} /> 
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
